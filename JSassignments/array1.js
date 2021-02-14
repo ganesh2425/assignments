30b529004bd3d3e2c6cf9ace32ec6943c41d08a9
ganesh2425 <saahoganesh023@gmail.com>
2021-02-14 12:19:45 +0530

    DONE

---
+++ JSassignments/array1.js
@@ -0,0 +1,12 @@
+let Mobiles = ['Samsung', 'Nokia', 'Apple', 'Intex'];
+
+console.log(Mobiles);
+
+let pos = 2
+let n = 2
+let removedItems = Mobiles.splice(pos,n);
+
+console.log(removedItems);
+
+console.log(Mobiles);
+
